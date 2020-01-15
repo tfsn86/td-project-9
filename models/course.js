347711d27d3d5a101ac2fb2d25b3f432a8e0fec2
tfsn <tfsn86@gmail.com>
2020-01-15 17:48:25 +0100

    Update course.js

--- models/course.js
+++ models/course.js
@@ -1,14 +1,18 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Course = sequelize.define('Course', {
-    userId: DataTypes.INTEGER,
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    estimatedTime: DataTypes.STRING,
-    materialsNeeded: DataTypes.STRING
-  }, {});
-  Course.associate = function(models) {
-    // associations can be defined here
-  };
-  return Course;
+	const Course = sequelize.define(
+		'Course',
+		{
+			userId: DataTypes.INTEGER,
+			title: DataTypes.STRING,
+			description: DataTypes.TEXT,
+			estimatedTime: DataTypes.STRING,
+			materialsNeeded: DataTypes.STRING
+		},
+		{}
+	);
+	Course.associate = function(models) {
+		// associations can be defined here
+	};
+	return Course;
 };

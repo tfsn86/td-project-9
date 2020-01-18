16632bf0d46221dc1199f021be23e4759648d41a
tfsn <tfsn86@gmail.com>
2020-01-18 21:15:13 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,17 +1,72 @@
 'use strict';
+
+const Sequelize = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
 	const User = sequelize.define(
 		'User',
 		{
-			firstName: DataTypes.STRING,
-			lastName: DataTypes.STRING,
-			emailAddress: DataTypes.STRING,
-			password: DataTypes.STRING
+			id: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			firstName: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				validate: {
+					notNull: {
+						msg: 'Please provide a value for "firstName"'
+					},
+					notEmpty: {
+						msg: 'Please provide a value for "firstName"'
+					}
+				}
+			},
+			lastName: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				validate: {
+					notNull: {
+						msg: 'Please provide a value for "lastName"'
+					},
+					notEmpty: {
+						msg: 'Please provide a value for "lastName"'
+					}
+				}
+			},
+			emailAddress: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				validate: {
+					notNull: {
+						msg: 'Please provide an "email address"'
+					},
+					notEmpty: {
+						msg: 'Please provide an "email address"'
+					}
+				}
+			},
+			password: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				validate: {
+					notNull: {
+						msg: 'Please provide a "password"'
+					},
+					notEmpty: {
+						msg: 'Please provide a "password"'
+					}
+				}
+			}
 		},
 		{}
 	);
-	User.associate = function(models) {
-		// associations can be defined here
+	User.associate = models => {
+		User.hasMany(models.Course, {
+			as: 'user',
+			foreignKey: { fieldName: 'userId', allowNull: false }
+		});
 	};
 	return User;
 };
